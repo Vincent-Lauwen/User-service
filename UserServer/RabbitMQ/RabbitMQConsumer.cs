924d477e22f605e07e335cebc20fe00136be7194
Vincent <427307@student.fontys.nl>
2022-05-18 14:01:10 +0200

    -

---
+++ UserServer/RabbitMQ/RabbitMQConsumer.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ
+{
+    public class RabbitMQConsumer : BackgroundService
+    {
+        private ConnectionFactory _factory;
+        private IConnection _connection;
+        private IModel _channel;
+
+        // initialize the connection, channel and queue inside the constructor to persist them for until the service (or the application) runs
+        public RabbitMQConsumer()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+            };
+            
+            _connection = _factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare("orders");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("start background service");
+            // when the service is stopping, dispose these references to prevent leaks
+            if (stoppingToken.IsCancellationRequested)
+            {
+                _channel.Dispose();
+                _connection.Dispose();
+                return Task.CompletedTask;
+            }
+
+            // create a consumer that listens on the channel (queue)
+            var consumer = new EventingBasicConsumer(_channel);
+
+            // handle the Received event on the consumer this is triggered whenever a new message is added to the queue by the producer
+            consumer.Received += (model, ea) =>
+            {
+                // read the message bytes
+                var body = ea.Body.ToArray();
+                var message = Encoding.Unicode.GetString(body);
+
+                Console.WriteLine(" [x] Received {0}", message);
+
+            };
+
+            _channel.BasicConsume(queue: "orders", autoAck: true, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UserServer/RabbitMQ/RabbitMQProducer.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ
+{
+    public class RabbitMQProducer : IMessageProducer
+    {
+        public void SendMessage<T>(T message)
+        {
+            var factory = new ConnectionFactory {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+            };
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.Unicode.GetBytes(json);
+
+            channel.BasicPublish(exchange: "", routingKey: "orders", body: body);
+        }
+    }
+}
