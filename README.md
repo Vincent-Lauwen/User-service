3778196c0511af5fe5e9b1d935128aff781b2098
Vincent <427307@student.fontys.nl>
2022-05-31 16:24:37 +0200

    -

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# User-service
+# User-Service
+
+## Manually use docker
+1. `docker-compose -f docker-compose.yml up` : start container in Docker
+
+## Need db context to migrate?
+1. `Add-Migration DBInit` &
+2. `Update-Database` to migrate models to database
+
+## Need to auth with docker?
+1. `echo githubtoken | docker login ghcr.io -u Vincent-Lauwen --password-stdin`
+
+## Problems while running?
+
+1. Remove `./output` (it will remove all databases)
--- UserServer/RabbitMQ/RabbitMQConsumer.cs
+++ UserServer/RabbitMQ/RabbitMQConsumer.cs
@@ -10,52 +10,67 @@ namespace RabbitMQ
         private IConnection _connection;
         private IModel _channel;
 
-        // initialize the connection, channel and queue inside the constructor to persist them for until the service (or the application) runs
+        //initialize the connection, channel and queue inside the constructor to persist them for until the service (or the application) runs
         public RabbitMQConsumer()
         {
-            _factory = new ConnectionFactory
+            try
             {
-                HostName = "localhost",
-                Port = 5672,
-                UserName = "guest",
-                Password = "guest",
-            };
-            
-            _connection = _factory.CreateConnection();
+                _factory = new ConnectionFactory
+                {
+                    HostName = "localhost",
+                    Port = 5672,
+                    UserName = "guest",
+                    Password = "guest",
+                };
 
-            _channel = _connection.CreateModel();
+                _connection = _factory.CreateConnection();
 
-            _channel.QueueDeclare("orders");
+                _channel = _connection.CreateModel();
+
+                _channel.QueueDeclare("orders");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            Console.WriteLine("start background service");
-            // when the service is stopping, dispose these references to prevent leaks
-            if (stoppingToken.IsCancellationRequested)
+            try
             {
-                _channel.Dispose();
-                _connection.Dispose();
+                Console.WriteLine("start background service");
+                // when the service is stopping, dispose these references to prevent leaks
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    _channel.Dispose();
+                    _connection.Dispose();
+                    return Task.CompletedTask;
+                }
+
+                // create a consumer that listens on the channel (queue)
+                var consumer = new EventingBasicConsumer(_channel);
+
+                // handle the received event on the consumer this is triggered whenever a new message is added to the queue by the producer
+                consumer.Received += (model, ea) =>
+                {
+                    // read the message bytes
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.Unicode.GetString(body);
+
+                    Console.WriteLine(" [x] received {0}", message);
+
+                };
+
+                _channel.BasicConsume(queue: "orders", autoAck: true, consumer: consumer);
+
                 return Task.CompletedTask;
             }
-
-            // create a consumer that listens on the channel (queue)
-            var consumer = new EventingBasicConsumer(_channel);
-
-            // handle the Received event on the consumer this is triggered whenever a new message is added to the queue by the producer
-            consumer.Received += (model, ea) =>
+            catch (Exception ex)
             {
-                // read the message bytes
-                var body = ea.Body.ToArray();
-                var message = Encoding.Unicode.GetString(body);
-
-                Console.WriteLine(" [x] Received {0}", message);
-
-            };
-
-            _channel.BasicConsume(queue: "orders", autoAck: true, consumer: consumer);
-
-            return Task.CompletedTask;
+                Console.WriteLine(ex);
+                return Task.CompletedTask;
+            }
         }
     }
 }
